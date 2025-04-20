d0a653286024cb68b14f15389b21cb8c86b1a628
Kelvin John Falk Szolnoky <me@kelszo.com>
2025-04-20 17:19:28 +0200

    chore: switch to pnpm and update dependencies

--- flake.nix
+++ flake.nix
@@ -1,43 +1,57 @@
 {
   inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
     devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   nixConfig = {
-    extra-trusted-public-keys =
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
-    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
       packages = forEachSystem (system: {
         devenv-up = self.devShells.${system}.default.config.procfileScript;
       });
 
-      devShells = forEachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
           default = devenv.lib.mkShell {
             inherit inputs pkgs;
-            modules = [{
-              packages = with pkgs; [ npm-check-updates ];
+            modules = [
+              {
+                packages = with pkgs; [ npm-check-updates ];
 
-              languages.typescript.enable = true;
+                languages.typescript.enable = true;
 
-              languages.javascript = {
-                enable = true;
-                yarn = {
+                languages.javascript = {
                   enable = true;
-                  install.enable = true;
+                  pnpm = {
+                    enable = true;
+                    install.enable = true;
+                  };
                 };
-              };
-            }];
+              }
+            ];
           };
-        });
+        }
+      );
     };
 }
